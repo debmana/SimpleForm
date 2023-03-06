fb335d8d01bd38086df32798cf52da11311da065
debmana <as.debora@gmail.com>
2023-03-06 15:52:54 -0700

    first commit for simple form

---
+++ src/main/java/com/deborasroka/simpleform/model/customer.java
@@ -0,0 +1,21 @@
+package com.deborasroka.simpleform.model;
+
+public class customer {
+	int ID;
+	String firstName;
+	String LastName;
+	String email;
+	String password;
+	String phone;
+	String address;
+	String city;
+	String country;
+	String website;
+	String gender;
+	String bday;
+	String mday;
+	String yday;
+	boolean privacyagr;
+	boolean offers;
+	
+}
